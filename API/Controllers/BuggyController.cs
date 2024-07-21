9300a06e8b3e01bb6fd9a2aa233625fe2033f164
ratnesh <t.rockey111@gmail.com>
2024-07-21 17:23:14 +0530

    error handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController(DataContext Context):ControllerBase
+    {
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetAuth()
+        {
+            return "secret text";
+        }
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = Context.Users.Find(-1);
+            if (thing == null) return NotFound();
+            return thing;
+        }
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var thing = Context.Users.Find(-1) ?? throw new Exception("a bad thing has happened");
+            return thing;
+        }
+        [HttpGet("bad-request")]
+        public ActionResult<AppUser> GetBadRequest()
+        {
+           
+            return BadRequest("this was not a good request");
+        }
+
+    }
+}
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,13 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptor/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass:'toast-button-right'
